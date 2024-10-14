3bc103ca1b9fd1b9fe2fc02bd973adc530669966
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2024-10-14 17:47:41 -0500

    added blog and about me links to nav, also created about me page.tsx

---
+++ app/about-me/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>about me</div>;
+};
+
+export default Page;
--- components/main/Nav.tsx
+++ components/main/Nav.tsx
@@ -3,11 +3,22 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="z-50 fixed top-0 w-screen max-w-[800px] flex justify-between items-center p-2 px-4 border-b bg-blur-10">
+    <nav className="z-50 fixed top-0 w-screen max-w-[800px] flex justify-between items-center p-2 px-6 border-b bg-blur-10">
       <Link className="font-bold text-2xl" href={"/"}>
         ITS
       </Link>
-      <ThemeToggler />
+      <div className="flex gap-6 items-center">
+        <Link className="hover:underline" href={"/about-me"}>
+          About Me
+        </Link>
+        <Link
+          className="hover:underline"
+          href={"http://localhost:3001/user/user_2iqJuHsepKWDsGxo2o6rczQpvYq"}
+        >
+          My Blogs
+        </Link>
+        <ThemeToggler />
+      </div>
     </nav>
   );
 };
