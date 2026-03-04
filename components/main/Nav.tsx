09a83f36984d74013d5ad6bb5450f0ce9f425240
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2026-03-04 09:39:41 -0600

    add chat route to mobile nav

--- components/main/Nav.tsx
+++ components/main/Nav.tsx
@@ -72,6 +72,12 @@ const Nav = () => {
                 >
                   C++ Zone
                 </Link>
+                <Link
+                  href="/about"
+                  className="px-3 py-2 rounded-lg hover:bg-slate-100 dark:hover:bg-slate-800 transition"
+                >
+                  Chat
+                </Link>
               </div>
               <span className="z-[40] relative">
                 <ThemeToggler isMobile={true} />
